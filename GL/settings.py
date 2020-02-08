74fec15c1d692cad52f2f25df79147893dbef8fa
NouAr <chaoukinouar@gmail.com>
2020-02-08 19:38:50 +0100

    My first commit

--- GL/settings.py
+++ GL/settings.py
@@ -133,7 +133,20 @@ USE_TZ = True
 LOGIN_REDIRECT_URL = 'account/'
 LOGOUT_REDIRECT_URL = '/'
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 import dj_database_url
 db_from_env = dj_database_url.config()
@@ -147,7 +160,7 @@ ALLOWED_HOSTS = ['*']
 SITE_ID = 1
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+STATIC_URL = '/static/'
 TEMPLATE_DIRS=(
 os.path.join(os.path.dirname(BASE_DIR),"static","templates"),
 )
