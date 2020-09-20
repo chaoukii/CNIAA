bb89d66283050a92e8b9b45ca92d859d09cfd8b9
chaoukii <34491573+chaoukii@users.noreply.github.com>
2020-09-20 11:12:31 +0100

    Update settings.py

--- GL/settings.py
+++ GL/settings.py
@@ -17,8 +17,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'chaoukinouar@gmail.com'
-EMAIL_HOST_PASSWORD = 'aqwa1234'
+EMAIL_HOST_USER = '<Your Email>'
+EMAIL_HOST_PASSWORD = '<Your Password>'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
